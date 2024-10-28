45ff8591cb6fafa1eb08bf192b238ef3b79153ac
yeonfish6040 <yeonfish6040@gmail.com>
2024-10-28 14:42:36 +0900

    fuck

--- rhythmlikeboom/rhythmlikeboom.cpp
+++ rhythmlikeboom/rhythmlikeboom.cpp
@@ -6,6 +6,7 @@
 #include <windows.h>
 #include <string.h>
 
+// global actions
 void printFile(std::string filename) {
     std::ifstream file(("./resources/text/" + filename + ".txt").c_str());
     char ch;
@@ -23,6 +24,35 @@ void rewrite() {
     SetConsoleCursorPosition(hConsole, coord);
 }
 
+// intro actions
+void drawLine(int loop, int LENGTH, int BAR_LENGTH) {
+    for (int i = 0; i < loop - BAR_LENGTH; i++) printf(" ");
+    for (int i = 0; i < (loop - BAR_LENGTH < 0) ? BAR_LENGTH-loop : BAR_LENGTH; i++) printf("#");
+    for (int i = 0; i < LENGTH - loop; i++) printf(" ");
+}
+
+// animation
+void introAnimation() {
+    int loop = 0;
+    while (true) {
+        const int BAR_LENGTH = 4;
+        const int LENGTH = 130;
+
+        if (loop > 130) loop = 0;
+        if (GetKeyState(VK_RETURN) & 0x8000) {
+            break;
+        }
+
+        rewrite();
+        drawLine(loop, LENGTH, BAR_LENGTH);
+        printFile("intro");
+        drawLine(loop, LENGTH, BAR_LENGTH);
+        Sleep(10);
+        loop++;
+    }
+}
+
+// initialize
 void initScreen() {
     HWND wh = GetConsoleWindow();
     MoveWindow(wh, 0, 0, 1920, 1000, TRUE);
@@ -30,21 +60,7 @@ void initScreen() {
     std::cout << "\n";
 }
 
-void introAnimation(int n) {
-
-}
-
 int main() {
     initScreen();
-
-    int n = 0;
-    int* np = &n;
-    while (true) {
-        if (GetKeyState(VK_RETURN) & 0x8000) {
-            break;
-        }
-        rewrite();
-        printFile("intro");
-        Sleep(10);
-    }
+    introAnimation();
 }
